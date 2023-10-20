644b6cf34214fff2c78d48a533d0ce7ff57890ba
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-10-20 11:05:32 -0700

    creates MyCar Model and adds dependencies to txt file

--- car_index/mycar/models.py
+++ car_index/mycar/models.py
@@ -1,3 +1,21 @@
+# <appname>/models.py
+
 from django.db import models
 
 # Create your models here.
+
+class MyCarModel(models.Model):
+
+  make = models.CharField(max_length = 200)
+  model = models.CharField(max_length = 200)
+  year = models.CharField(max_length = 200)
+  nickname = models.CharField(max_length = 200)
+  
+
+  def __str__(self):
+    return self.title
+  
+
+
+
+
