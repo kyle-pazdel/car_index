87c8565dabd15cd343409d8f98436a4abee48bac
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-10-20 15:21:32 -0700

    Configures update url in mycar urls

--- car_index/mycar/urls.py
+++ car_index/mycar/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns = {
   path('', views.list_view),
-  path('<id>', views.detail_view),
   path("create", views.create_view),
+  path('<id>', views.detail_view),
+  path('<id>/update', views.update_view),
 }
 
 
