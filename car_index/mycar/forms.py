37791f45bf38a4974159c86b817a85e2d9b32be6
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-10-20 11:23:15 -0700

    creates form for MyCar Model

---
+++ car_index/mycar/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import MyCarModel
+
+class MyCarForm(forms.ModelForm):
+  class Meta:
+    model = MyCarModel
+
+    fields = [
+      "make",
+      "model",
+      "year",
+      "nickname"
+    ]
+
+
+
+
