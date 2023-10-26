03aac2581cecd7dcd2a5e99889962f58b9ef5371
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-10-26 14:22:57 -0700

    Uses DetailView class in views, uses pk and path() method in url patterns for detail view

--- car_index/mycar/urls.py
+++ car_index/mycar/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = {
   path('', views.list_view),
   path("create", views.create_view),
-  path('<int:id>', views.detail_view),
+  path('<slug:pk>', views.DetailView.as_view(), name="detail_view"),
   path('<int:id>/update', views.update_view),
   path('<int:id>/delete', views.delete_view),
 }
--- car_index/mycar/views.py
+++ car_index/mycar/views.py
@@ -1,11 +1,11 @@
 # <appname>/views.py
 from django.shortcuts import (get_object_or_404, render, HttpResponseRedirect)
+from django.views.generic.detail import DetailView
 
 from .models import MyCarModel
 from .forms import MyCarForm
-import os
+import os, requests
 
-secret = os.environ['API_KEY']
 
 def create_view(request):
   context = {}
@@ -17,17 +17,40 @@ def create_view(request):
   context['form'] = form
   return render(request, "create_view.html", context)
 
+
 def list_view(request):
   context = {}
   context["dataset"] = MyCarModel.objects.all()
 
   return render(request, "list_view.html", context)
 
-def detail_view(request, id):
-  context = {}
-  context["data"] = MyCarModel.objects.get(id = id)
 
-  return render(request, "detail_view.html", context)
+class DetailView(DetailView):
+  # context = {}
+  # context["data"] = MyCarModel.objects.get(id = id)
+
+  # api_url = 'https://api.api-ninjas.com/v1/cars?model={}'.format()
+  api_url = 'https://api.api-ninjas.com/v1/cars?model={"civic"}'
+  response = requests.get(api_url, headers={'X-Api-Key': os.environ['API_KEY']})
+
+  template_name="detail_view.html"
+  model = MyCarModel
+  def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      # context['data'] = MyCarModel.objects.all()
+      # context["car_data"] = {"test":"TEST"}
+      return context
+
+
+  # if response.status_code == requests.codes.ok:
+  #   context["car_data"] = response.text
+  # else:
+  #   context["car_data"] = response.status_code, response.text    
+
+  
+
+  # return render(request, "detail_view.html", context)
+  
 
 def update_view(request, id):
   context = {}
@@ -42,6 +65,7 @@ def update_view(request, id):
   context["form"] = form
 
   return render(request, "update_view.html", context)
+
 
 def delete_view(request, id):
   context = {}
