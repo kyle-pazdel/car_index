4bcb92aa686972a890a47b381ac93d0257314bf8
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-11-02 13:38:11 -0700

    Corrects url formatting issue with trailing backslash

--- car_index/mycar/urls.py
+++ car_index/mycar/urls.py
@@ -4,11 +4,11 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = {
-  path('create', views.create_view),
+  path('create/', views.create_view),
   path('', views.list_view),
-  path('<slug:pk>', views.MyCarDetailView.as_view(), name="detail_view"),
-  path('<int:id>/update', views.update_view),
-  path('<int:id>/delete', views.delete_view),
+  path('<slug:pk>/', views.MyCarDetailView.as_view(), name="detail_view"),
+  path('<int:id>/update/', views.update_view),
+  path('<int:id>/delete/', views.delete_view),
 }
 
 
