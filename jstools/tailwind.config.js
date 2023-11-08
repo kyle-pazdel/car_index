13c4383ac1174497b206a3020f81f6fdd5faf2a0
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-11-07 17:31:36 -0800

    Configures package.json and tailwind.config.js

--- jstools/tailwind.config.js
+++ jstools/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: false, //true for production build
+    content: ["../**/templates/*.html", "../**/templates/**/*.html"],
+  },
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
-}
-
+};
